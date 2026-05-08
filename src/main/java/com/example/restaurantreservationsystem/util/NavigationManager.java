cf43bd6bdb80bc34957f14f0c27450223b6b97bd
ahmadAledlbe <63427093+ahmadAledlbe@users.noreply.github.com>
2026-05-08 14:51:22 +0300

    Refactor NavigationManager to C# and add forms

--- src/main/java/com/example/restaurantreservationsystem/util/NavigationManager.java
+++ src/main/java/com/example/restaurantreservationsystem/util/NavigationManager.java
@@ -1,27 +1,123 @@
-package com.example.restaurantreservationsystem.util;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
 
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+public class NavigationManager
+{
+    private static NavigationManager _instance;
+    private static readonly object _lock = new object();
+    private Form _primaryForm;
 
-// Centralised router that swaps FXML scenes on the primary stage
-public class NavigationManager {
+    protected NavigationManager() { }
 
-    private static NavigationManager instance;
-    private Stage primaryStage;
+    public static NavigationManager GetInstance()
+    {
+        if (_instance == null)
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                    _instance = new NavigationManager();
+            }
+        }
+        return _instance;
+    }
 
-    public static NavigationManager getInstance()        { return null; }
+    public void SetPrimaryForm(Form form)
+    {
+        _primaryForm = form;
+    }
 
-    public void setPrimaryStage(Stage stage)             {}
+    public void NavigateToDashboard()
+    {
+        LoadForm(new DashboardForm());
+    }
 
-    public void navigateToDashboard()                    {}
+    public void NavigateToCustomers()
+    {
+        LoadForm(new CustomersForm());
+    }
 
-    public void navigateToCustomers()                    {}
+    public void NavigateToReservations()
+    {
+        LoadForm(new ReservationsForm());
+    }
 
-    public void navigateToReservations()                 {}
+    public void NavigateToTables()
+    {
+        LoadForm(new TablesForm());
+    }
 
-    public void navigateToTables()                       {}
+    public void NavigateToStaff()
+    {
+        LoadForm(new StaffForm());
+    }
 
-    public void navigateToStaff()                        {}
+    private void LoadForm(Form form)
+    {
+        if (_primaryForm == null)
+            throw new InvalidOperationException("Primary form has not been set.");
 
-    private Scene loadScene(String fxmlPath)             { return null; }
+        List<Form> formsToClose = new List<Form>();
+        foreach (Form f in Application.OpenForms)
+            if (f != _primaryForm)
+                formsToClose.Add(f);
+
+        foreach (Form f in formsToClose)
+            f.Close();
+
+        form.Show();
+        form.BringToFront();
+    }
+}
+
+public class DashboardForm : Form
+{
+    public DashboardForm()
+    {
+        this.Text = "Dashboard";
+        this.Size = new Size(800, 600);
+    }
+}
+
+public class CustomersForm : Form
+{
+    public CustomersForm()
+    {
+        this.Text = "Customers";
+        this.Size = new Size(800, 600);
+    }
+}
+
+public class ReservationsForm : Form
+{
+    public ReservationsForm()
+    {
+        this.Text = "Reservations";
+        this.Size = new Size(800, 600);
+    }
+}
+
+public class TablesForm : Form
+{
+    public TablesForm()
+    {
+        this.Text = "Tables";
+        this.Size = new Size(800, 600);
+    }
+}
+
+public class StaffForm : Form
+{
+    public StaffForm()
+    {
+        this.Text = "Staff";
+        this.Size = new Size(800, 600);
+    }
+}
+
+public class NavigationManager_TestHelper : NavigationManager
+{
+    public NavigationManager_TestHelper() { }
 }
